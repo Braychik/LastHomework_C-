bf7315b1ebce515f866ad9e11bc1b87070aa5242
Николай <ibraevnikolay@mail.ru>
2023-03-21 19:46:14 +0500

    Создали функцию перебирания элементов из массива

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,16 @@
-﻿string [] array = ["hi", "hello", "good", "morning", "I", "am", "do", "it"];
-string [] 
+﻿string [] array = new string [8] {"hi", "hello", "good", "morning", "I", "am", "do", "it"}; 
+string [] OtherArray(string[] array)
+{
+    string[] result = new string[array.Length];
+    int count = 0;
+    for(int i = 0; i <= array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        result[count] = array[i];
+        count++;
+    }
+return result;
+} 
+
+
+
