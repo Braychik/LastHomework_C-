603b6be71e2560e13c0187045694d8e4f87cb252
Николай <ibraevnikolay@mail.ru>
2023-03-21 20:32:02 +0500

    Другое решение - неправильное

--- task/Program.cs
+++ task/Program.cs
@@ -1,24 +1,22 @@
-﻿string [] array = new string [8] {"hi", "hello", "good", "morning", "I", "am", "do", "it"}; 
-string [] OtherArray(string[] array)
+﻿Console.Clear();
+string [] array = new string [8] {"hi", "hello", "good", "morning", "I", "am", "do", "it"}; 
+string[] result = OtherArray(array);
+
+string[] OtherArray(string[] array)
 {
-    string[] result = new string[array.Length];
+    string[] array1 = new string[array.Length];
     int count = 0;
-    for(int i = 0; i <= array.Length; i++)
+    for(int i = 0; i < array.Length; i++)
     {
         if(array[i].Length <= 3)
-        result[count] = array[i];
-        count++;
+        {
+            array1[count] = array[i];
+            count++;
+        }
     }
-return result;
+    return array1;
 } 
+Console.WriteLine(result);
 
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
-PrintArray(array);
+
 
