f3aa78a2338d334f5791c91b9ab080fd9e5c3150
Николай <ibraevnikolay@mail.ru>
2023-03-21 19:47:00 +0500

    добавили PrintArray для вывода массива

--- task/Program.cs
+++ task/Program.cs
@@ -12,5 +12,13 @@ string [] OtherArray(string[] array)
 return result;
 } 
 
-
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(array);
 
